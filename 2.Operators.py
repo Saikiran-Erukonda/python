7c0358731eca336e937ded83785b4f0c40061468
ERUKONDA SAIKIRAN <148369747+Saikiran-Erukonda@users.noreply.github.com>
2024-09-13 19:27:55 +0530

    Add files via upload

---
+++ 2.Operators.py
@@ -0,0 +1,62 @@
+# 2.OPERATORS
+"""
+1. Write a function for arithmetic operators(+,-,*,/)
+"""
+
+#we need to give the 2 numbers as input and the respective outputs will be displayed after arithmetic operations
+
+i_a = eval(input("enter first number: "))
+i_b = eval(input("enter second number: "))
+print("The two numbers are","\ni_a =",i_a,"\ni_b = ",i_b)
+print("o_addition =", i_a + i_b)
+print("o_subtraction =",i_a - i_b)
+print("o_multiplication =", i_a * i_b)
+print("o_division = ",i_a/i_b)
+
+"""
+2. Write a method for increment and decrement operators(++, --)
+"""
+# python does not have increment and decrement operator directly. Use for loop for increment and decrement operations
+i = 0
+k = 20
+for i in range(0,5):
+  print(i)  # prints value of i upto 4
+for k in range(20,15,-1):
+  print(k)  # prints value of k upto 16
+
+""" 3. Write a program to find the two numbers equal or not."""
+
+value = 100
+number = 100
+print("\nvalue = ",value,"\nnumber = ",number)
+print("Is the value equal to number :",value == number)
+
+
+""" 4. Program for relational operators (<,<==, >, >==)"""
+
+value1 = 100
+number1= 120
+print("\nvalue1 = ",value1,"\nnumber1 = ",number1)
+print("Is the value1 equal to number1: ",value1 == number1)                    #value1 == number1
+print("Is the value1 less than number1: ",value1 < number1)                    #value1 < number1
+print("Is the value1  less than or equal to number1 :",value1 <=  number1)     #value1 <= number1
+print("Is the value1 greater than number1 :",value > number)                   #value1 > number1
+print("Is the value1 greater than or equal to number1: ",value1 >= number1)    #value1 >= number1
+
+"""
+5. Print the smaller and larger number
+"""
+
+a = eval(input('Enter first number: '))
+b = eval(input('Enter second number: '))
+#To print larger number
+if a > b: 
+     print(a, "is larger ")
+else:
+    print(b, " is larger ")
+#To print smaller number
+if a < b:
+     print(a, "is smaller ")
+else:
+    print(b, " is smaller ")  
+
---
+++ 3.Loops.py
@@ -0,0 +1,111 @@
+#1. Write a program to print “Bright IT Career” ten times using for loop
+
+i=0
+for i in range(10):
+    print("Bright IT Career")
+# 2. Write a java program to print 1 to 20 numbers using the while loop.
+j=1
+while j<=20:
+    print(j)
+    j += 1
+
+# 3. Program to equal operator and not equal operators
+
+x,y,z = 10, 20 , 30
+if(x == 10 and z != 20):
+    print("equal operator and not equal operators are working")
+
+
+# 4. Write a program to print the odd and even numbers in given range.
+print("\n Odd and even numbers in given range")
+max_num = int(input('enter the max number: '))
+even_num = ['Even numbers :']
+odd_num = ['Odd numbers :']
+for num in range(1,max_num):
+    if(num%2 == 0):
+        even_num.append(num)
+    elif(num%2 == 1):
+        odd_num.append(num)
+print('Odd numbers',odd_num[1:])
+print('Even numbers',even_num[1:])
+
+#5. Write a program to print largest number among three numbers
+
+x,y,z = 10,50,15
+large = 0
+if x >= y and x >= z:
+    large = x
+elif y>=z and y>=x:
+    large = y
+elif z>=x and z>=y:
+    large = z
+print("\nLargest among the numbers",x,y,z)
+print(large)
+
+'''6. Write a program to print even number between 10 and 20 using while'''
+N = 10
+print('\neven numbers using while loop between 10 and  20')
+while N in range(20):
+    N += 1
+    if(N%2 == 0):
+        print(N,end = " ")
+
+'''7. Write a program to print 1 to 10 using the do-while loop statement'''
+
+    # do-while loop doesn't exists in python
+
+
+"""8. Write a program to find Armstrong number or not"""
+
+"""Armstrong number : a number that is equal to the sum of power_base_n of its own digits..
+   for example : 153, 1^3 + 5^3 + 3^3 = 153
+                1634, 1^4 + 6^4 + 3^4 + 4^4 = 1634
+"""
+print("\n ------------------- Armstrong number ----------------------")
+a_n = input('enter a number: ')
+p = len(a_n)
+temp = []
+for i in a_n:
+    temp.append(int(i)**p)
+if sum(temp)== int(a_n):
+    print("Given number",a_n,"is Armstrong number :)")
+else:
+    print("Given number ",a_n,"is Not a Armstrong number :(")
+
+"""9. Write a program to find the prime or not."""
+print("\n----------- Prime number or Not -----------------")
+i_num = int(input('enter a number: '))
+value = 0
+f = []
+for value in range(2,i_num):
+    if(i_num % value == 0):
+        f.append(value)
+#print(f)
+if f == []:
+    print(i_num," is a prime")
+else:
+    print(i_num," is not prime")
+    
+
+
+"""10. Write a program to palindrome or not"""
+print("\n----------- Palindrome or Not -----------------")
+word = input('enter something: ')
+reverse_word = ""
+    #print(word[0:])
+for j in word:
+    reverse_word = j + reverse_word
+    #print(reverse_word)
+if (reverse_word==word):
+    print("the given string '",word,"'is palindrome")
+else:
+    print("the given string '",word,"'is not palindrome")
+    
+
+    
+
+
+
+
+
+
